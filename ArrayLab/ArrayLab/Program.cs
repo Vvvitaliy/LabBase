02b176617fa295ba413db75d6fd3a9e2e6e3469c
Vitaly <vitaliy.sharov@hotmail.com>
2020-11-19 21:47:44 +0300

    Add lab tasks

---
+++ ArrayLab/ArrayLab/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArrayLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = MyArray.Initialise(10);
+            MyArray.Show(array);
+
+            MyArray.Sort(array);
+            MyArray.Show(array);
+        }
+    }
+}
