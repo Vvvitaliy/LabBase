f00768be5f7ebeab1818a50bad55b3af6122a501
Vitaly <vitaliy.sharov@hotmail.com>
2020-11-27 21:47:34 +0300

    Add OOP lab

---
+++ OOP/OOP/Vehicle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    public abstract class Vehicle
+    {
+        private string model;
+        private int wheelsNumber;
+        private int horsePower;
+        private int maxSpeed;
+        private string status;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int WheelsNumber 
+        {
+            get { return wheelsNumber; }
+            protected set { wheelsNumber = value; }
+        }
+        public int HorsePower
+        {
+            get { return horsePower; }
+            set { horsePower = value; }
+        }
+        public int MaxSpeed
+        {
+            get { return maxSpeed; }
+            set { maxSpeed = value; }
+        }
+        public string Status
+        {
+            get { return status; }
+            private set { status = value; }
+        }
+
+        public void Drive()
+        {
+            Status = "Едет";
+        }
+        public void Stop()
+        {
+            Status = "Стоит";
+        }
+
+        public abstract void ShowAllInfo();
+    }
+}
